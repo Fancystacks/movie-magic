4c08a506af289d266b7308229fdd31a5727b030a
Leah Migacz <lmigacz@msudenver.edu>
2020-04-07 21:57:58 -0600

    began to create components

---
+++ src/components/search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Search() {
+    return (
+        <div className="search-div">
+            <input type="text" className="search-box"
+       placeholder="Search movie database for a title... "/>
+        </div>
+    )
+}
+
+export default Search;
